0b7f97d53fe9512054811eb66c5421e60c5e68fb
Kshiteesh <kshiteeshsn@gmail.com>
2019-08-19 17:22:34 +0530

    updated api key

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,25 +1,30 @@
 // Initialize and add the map
 function initMap() {
   // Your location
-  const loc = { lat: 12.904096, lng: 77.597213 };
+  const loc = {
+    lat: 12.904096,
+    lng: 77.597213
+  };
   // Centered map on location
   const map = new google.maps.Map(document.querySelector('.map'), {
     zoom: 14,
     center: loc
   });
   // The marker, positioned at location
-  const marker = new google.maps.Marker({ position: loc, map: map });
+  const marker = new google.maps.Marker({
+    position: loc,
+    map: map
+  });
 }
 
 // Smooth Scrolling
-$('#navbar a, .btn').on('click', function(event) {
+$('#navbar a, .btn').on('click', function (event) {
   if (this.hash !== '') {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate(
-      {
+    $('html, body').animate({
         scrollTop: $(hash).offset().top - 100
       },
       800
@@ -28,7 +33,7 @@ $('#navbar a, .btn').on('click', function(event) {
 });
 
 // Sticky menu background
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   if (window.scrollY > 150) {
     document.querySelector('#navbar').style.opacity = 0.9;
   } else {
